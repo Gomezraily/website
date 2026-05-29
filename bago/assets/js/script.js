e25827a644584b9d702ff40bd51c85add3df41b3
Gomezraily <166313459+Gomezraily@users.noreply.github.com>
2026-05-29 16:43:45 +0800

    Add files via upload

---
+++ bago/assets/js/script.js
@@ -0,0 +1,1 @@
+console.log("Chrome Hearts Website Loaded");
---
+++ bago/includes/body.php
@@ -0,0 +1,6 @@
+<!-- CORNER DESIGNS -->
+
+<span class="corner top-left">✦</span>
+<span class="corner top-right">✦</span>
+<span class="corner bottom-left">✦</span>
+<span class="corner bottom-right">✦</span>
